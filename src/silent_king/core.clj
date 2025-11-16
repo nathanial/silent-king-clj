8446a09ea42ebb843f0a2c107e9f837b83b44fef
Nathanial Hartman <nathanial.hartman@gmail.com>
2025-11-16 12:35:11 -0700

    fun with stars

--- src/silent_king/core.clj
+++ src/silent_king/core.clj
@@ -2,10 +2,23 @@
   (:import [org.lwjgl.glfw GLFW GLFWErrorCallback]
            [org.lwjgl.opengl GL GL11 GL30]
            [org.lwjgl.system MemoryUtil]
-           [io.github.humbleui.skija Canvas Color4f Paint Surface DirectContext BackendRenderTarget FramebufferFormat ColorSpace SurfaceOrigin SurfaceColorFormat Font Typeface]
-           [io.github.humbleui.types Rect]))
+           [io.github.humbleui.skija Canvas Color4f Paint Surface DirectContext BackendRenderTarget FramebufferFormat ColorSpace SurfaceOrigin SurfaceColorFormat Font Typeface Image Data]
+           [io.github.humbleui.types Rect]
+           [java.io File]))
 
 (set! *warn-on-reflection* true)
+
+(defn load-star-images []
+  (println "Loading star images...")
+  (let [star-files (sort (.listFiles (File. "assets/stars")))
+        ;; Load a selection of stars (every 8th one to get ~6 stars)
+        selected-files (take 6 (take-nth 8 star-files))]
+    (vec (for [^File file selected-files]
+           (let [data (Data/makeFromFileName (.getPath file))
+                 image (Image/makeFromEncoded (.getBytes data))]
+             (.close data)
+             {:image image
+              :path (.getName file)})))))
 
 (defn init-glfw []
   (println "Initializing GLFW...")
@@ -64,54 +77,57 @@
      SurfaceColorFormat/RGBA_8888
      (ColorSpace/getSRGB))))
 
-(defn draw-rotating-square [^Canvas canvas x y size angle color]
-  (let [paint (doto (Paint.)
-                (.setColor (unchecked-int color)))]
-    (.save canvas)
-    (.translate canvas x y)
-    (.rotate canvas angle)
-    (.drawRect canvas (Rect/makeXYWH (- (/ size 2)) (- (/ size 2)) size size) paint)
-    (.restore canvas)
-    (.close paint)))
+(defn draw-rotating-star [^Canvas canvas ^Image image x y size angle]
+  (.save canvas)
+  (.translate canvas x y)
+  (.rotate canvas angle)
+  (let [img-width (.getWidth image)
+        img-height (.getHeight image)
+        scale (/ size (max img-width img-height))
+        scaled-width (* img-width scale)
+        scaled-height (* img-height scale)]
+    (.drawImageRect canvas image
+                    (Rect/makeXYWH 0 0 img-width img-height)
+                    (Rect/makeXYWH (- (/ scaled-width 2))
+                                   (- (/ scaled-height 2))
+                                   scaled-width
+                                   scaled-height)))
+  (.restore canvas))
 
-(defn draw-frame [^Canvas canvas width height time]
+(defn draw-frame [^Canvas canvas width height time star-images]
   ;; Clear background
-  (.clear canvas (unchecked-int 0xFF1a1a2e))
+  (.clear canvas (unchecked-int 0xFF0a0a1e))
 
   (let [center-x (/ width 2)
         center-y (/ height 2)
-        rotation (* time 50) ;; degrees per second
-        orbit-radius 150]
+        rotation (* time 30) ;; degrees per second
+        orbit-radius 200]
 
-    ;; Draw multiple rotating squares in a circular pattern
-    (doseq [i (range 6)]
-      (let [angle (* i (/ 360 6))
+    ;; Draw multiple rotating stars in a circular pattern
+    (doseq [i (range (count star-images))]
+      (let [star-data (nth star-images i)
+            star-image (:image star-data)
+            angle (* i (/ 360 (count star-images)))
             rad-angle (* angle (/ Math/PI 180))
             x (+ center-x (* orbit-radius (Math/cos rad-angle)))
             y (+ center-y (* orbit-radius (Math/sin rad-angle)))
-            size 80
-            color (case i
-                    0 0xFF00d4ff ;; cyan
-                    1 0xFFff006e ;; pink
-                    2 0xFF8338ec ;; purple
-                    3 0xFFfb5607 ;; orange
-                    4 0xFFffbe0b ;; yellow
-                    5 0xFF3a86ff ;; blue
-                    0xFFffffff)]
-        (draw-rotating-square canvas x y size (+ rotation (* i 15)) color)))
+            size 120]
+        (draw-rotating-star canvas star-image x y size (+ rotation (* i 20)))))
 
-    ;; Draw center rotating square
-    (draw-rotating-square canvas center-x center-y 120 rotation 0xFFffffff)
+    ;; Draw center rotating star
+    (when (seq star-images)
+      (let [center-star (:image (first star-images))]
+        (draw-rotating-star canvas center-star center-x center-y 180 rotation)))
 
     ;; Draw title
     (let [paint (doto (Paint.)
                   (.setColor (unchecked-int 0xFFffffff)))
           font (Font. (Typeface/makeDefault) (float 32))]
-      (.drawString canvas "Silent King - Skija Animation" (float 20) (float 40) font paint)
+      (.drawString canvas "Silent King - Star Animation" (float 20) (float 40) font paint)
       (.close font)
       (.close paint))))
 
-(defn render-loop [window context surface-atom]
+(defn render-loop [window context surface-atom star-images]
   (println "Starting render loop...")
   (let [start-time (System/nanoTime)]
     (loop []
@@ -137,7 +153,7 @@
           ;; Render with Skija
           (let [^Surface surface @surface-atom
                 ^Canvas canvas (.getCanvas surface)]
-            (draw-frame canvas fb-width fb-height current-time)
+            (draw-frame canvas fb-width fb-height current-time star-images)
             (.flush surface))
 
           ;; Swap buffers and poll events
@@ -146,12 +162,14 @@
 
         (recur)))))
 
-(defn cleanup [window context surface-atom]
+(defn cleanup [window context surface-atom star-images]
   (println "Cleaning up...")
   (when @surface-atom
     (.close ^Surface @surface-atom))
   (when context
     (.close ^DirectContext context))
+  (doseq [star-data star-images]
+    (.close ^Image (:image star-data)))
   (when window
     (GLFW/glfwDestroyWindow window))
   (GLFW/glfwTerminate))
@@ -160,16 +178,19 @@
   (println "Starting Silent King...")
   (let [window (atom nil)
         context (atom nil)
-        surface (atom nil)]
+        surface (atom nil)
+        star-images (atom [])]
     (try
       (init-glfw)
-      (reset! window (create-window 1024 768 "Silent King - Skija Animation"))
+      (reset! window (create-window 1024 768 "Silent King - Star Animation"))
       (reset! context (create-skija-context))
-      (render-loop @window @context surface)
+      (reset! star-images (load-star-images))
+      (println "Loaded" (count @star-images) "star images")
+      (render-loop @window @context surface @star-images)
       (catch Exception e
         (println "Error:" (.getMessage e))
         (.printStackTrace e))
       (finally
-        (cleanup @window @context surface))))
+        (cleanup @window @context surface @star-images))))
   (println "Goodbye!")
   (System/exit 0))
